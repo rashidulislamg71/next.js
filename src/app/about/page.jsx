db5e75078c23bb536c8e16081d8b45d01815ad22
rashidulislamg71 <jrashidulislam0103@gmail.com>
2026-04-22 19:25:04 +0600

    learn next.js image optimization

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,19 @@
 
 import React from 'react'
+import Image from 'next/image'
 
 function AboutPage() {
   return (
-    <div>AboutPage</div>
+    <div>
+
+     <Image
+      src="/img-2.jfif"
+      alt="Picture of the author"
+      width={500}
+      height={500}
+    />
+
+    </div>
   )
 }
 
